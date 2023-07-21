305e70f65bf3110a9caa830c2b1517cf058a6c05
Sébastien Perpignane <sebastien.perpignane@gmail.com>
2023-07-21 23:37:55 +0200

    feat/sdkman installation ok+groovy installation ok

--- setup-groovy/__tests__/main.test.ts
+++ setup-groovy/__tests__/main.test.ts
@@ -1,29 +1,31 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import {expect, test, beforeEach} from '@jest/globals'
+import {installSdkman, SDKMAN_DIR} from '../src/main'
+import * as fs from 'fs'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
+const customInstallDir = '/home/derek/customsdkmandir'
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+beforeEach(() => {
+  fs.rmSync(SDKMAN_DIR, {recursive: true, force: true})
+  fs.rmSync(customInstallDir, {recursive: true, force: true})
+});
+
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+test('test runs', async () => {
+  process.env['INPUT_GROOVY_VERSION'] = '4.0.13'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
   console.log(cp.execFileSync(np, [ip], options).toString())
+})
+
+test('install sdkman', async () => {
+
+  await installSdkman(customInstallDir)
+  expect(fs.existsSync(customInstallDir))
 })
--- setup-groovy/src/main.ts
+++ setup-groovy/src/main.ts
@@ -1,31 +1,95 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
-import {wait} from './wait'
+import * as fs from 'fs'
+
+export const SDKMAN_DIR = '/home/derek/sdkmantest'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let groovyVersion = core.getInput("groovy-version")
+    
+    let sdkmanInstallDir = core.getInput("sdkman-install-dir")
+    if (!sdkmanInstallDir) {
+      sdkmanInstallDir = SDKMAN_DIR
+    }
+    
+    let sdkmanExitCode = await installSdkman(sdkmanInstallDir)
 
-    extractGroovyDependencies()
+    if (sdkmanExitCode) {
+      core.setFailed("SDKMAN! installation: KO (error code: " + sdkmanExitCode + ")")
+      return
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    console.log("SDKMAN! installation: OK")
+
+    await installGroovy(groovyVersion)
+
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
 function extractGroovyDependencies(): void {
-  exec.exec("groovy GrapeDependencies.groovy")
+  exec.exec('groovy', ['GrapeDependencies.groovy'])
 }
 
-function installSdkman(): void {
-  exec.exec("curl -s 'https://get.sdkman.io' | bash")
+// TODO 
+
+// FIXME: throw error if something goes bad instead of returning exit number
+export async function installSdkman(sdkmanInstallDir: string): Promise<number> {
+  let curlOutput = await getBashSdkmanInstallationScript()
+  let installScriptExitCode = await runSdkmanInstallScript(sdkmanInstallDir, curlOutput.stdout.toString())
+  configureSdkManForAutoAnswer(sdkmanInstallDir)
+  return installScriptExitCode
 }
 
+async function getBashSdkmanInstallationScript(): Promise<exec.ExecOutput> {
+  return await exec.getExecOutput('curl', [
+    '-s',
+    'https://get.sdkman.io?rcupdate=false'
+  ])
+}
 
-run()
+function configureSdkManForAutoAnswer(sdkmanInstallDir: string) {
+  console.info("Configuring SDKMAN! in non interactive mode...")
+  const sdkmanConfigFilePath = sdkmanInstallDir + '/etc/config'
+  const allFileContents = fs.readFileSync(sdkmanConfigFilePath, 'utf-8');
+  const newSdkManConfig = allFileContents.split(/\r?\n/).map(line => {
+    if (line.startsWith('sdkman_auto_answer=')) {
+      return 'sdkman_auto_answer=true'
+    }
+    else {
+      return line
+    }
+  }).join('\n')
+  fs.writeFileSync(sdkmanConfigFilePath, newSdkManConfig)
+  console.info("Configuring SDKMAN! in non interactive mode - done")
+}
+
+async function runSdkmanInstallScript(sdkmanInstallDir: string, scriptStr: string): Promise<number> {
+  const bashOptions = {
+    input: Buffer.from(scriptStr),
+    env: {
+      SDKMAN_DIR: sdkmanInstallDir
+    }
+  }
+
+  return await exec.exec('bash', [], bashOptions)
+}
+
+async function installGroovy(groovyVersion: string) {
+  console.log('Installing groovy...')
+  let output = await exec.getExecOutput('bash', [
+    '-c',
+    "source " + SDKMAN_DIR + "/bin/sdkman-init.sh && sdk install groovy " + groovyVersion
+  ])
+  console.log(output)
+
+  console.log('Installing groovy - done')
+
+}
+
+if (require.main === module) {
+  run();
+}
