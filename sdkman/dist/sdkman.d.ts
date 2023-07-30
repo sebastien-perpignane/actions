d489a9b35d0f80b598fd14497c23d7dc154ba4ed
Sébastien Perpignane <sebastien.perpignane@gmail.com>
2023-07-30 19:13:06 +0200

    feat/setup-sdkman as a  module and an action - fix

---
+++ sdkman/dist/sdkman.d.ts
@@ -0,0 +1,22 @@
+export declare const SDKMAN_DIR: string;
+export interface Candidate {
+    name: string;
+}
+export declare class SdkMan {
+    private installDir;
+    constructor(installDir: string);
+    installSdkMan(): Promise<number>;
+    private getBashSdkmanInstallationScript;
+    private runSdkmanInstallScript;
+    configureSdkManForAutoAnswer(): void;
+    uninstall(candidate: string, version: string, force?: boolean): Promise<void>;
+    installCandidateAndAddToPath(candidate: Candidate, version: string): Promise<void>;
+    private candidateDir;
+    private candidateCurrentDir;
+    private candidateVersionDir;
+    isInstalled(): boolean;
+    isCandidateInstalled(candidate: Candidate): boolean;
+    isCandidateVersionInstalled(candidate: Candidate, version: string): boolean;
+    private runCommand;
+    candidatesDir(): string;
+}
